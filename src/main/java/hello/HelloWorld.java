2e85e4699863c0829ad88536150381c73ff6a47a
Sanjeev Ishwar <sanjeevkr4201@gmail.com>
2025-07-14 01:17:43 +0530

    jenkins test

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -5,6 +5,7 @@ import org.joda.time.LocalTime;
 public class HelloWorld {
     public static void main(String[] args) {
       LocalTime currentTime = new LocalTime();
+      System.out.println("Some modification to test continuous integration in Jenkins");
 		  System.out.println("The current local time is: " + currentTime);
 
         Greeter greeter = new Greeter();
